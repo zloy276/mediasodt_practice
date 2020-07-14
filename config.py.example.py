ad12202340cfa82d5636c57ff78dbc3ef3b5bee8
zloy276 <52476650+zloy276@users.noreply.github.com>
2020-07-14 20:37:03 +0400

    1.1

---
+++ config.py.example.py
@@ -0,0 +1,4 @@
+t_api = '1272215182:AAHjwYdyiDyyW_rs4yuFIlRG_f5-ekK9O98'
+w_api = 'fdd26f8f1df5fc938fc402b59ee614c1'
+commands = ['/help', '/add', '/change', '/remove', ]
+#решил оставить все токены чтобы не затрудняло проверку поиск токена для проверки погоды
